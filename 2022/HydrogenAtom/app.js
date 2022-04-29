80072592a745bbbd2823c4b6a5ca56c51d3c977d
xam4lor <xam4lor@gmail.com>
2022-04-29 14:46:08 +0200

    Added GPU meta

--- 2022/HydrogenAtom/app.js
+++ 2022/HydrogenAtom/app.js
@@ -1,4 +1,10 @@
 async function startProgram() {
+    // Add loading GPU module
+    const otMeta = document.createElement('meta');
+    otMeta.httpEquiv = 'origin-trial';
+    otMeta.content = 'At1jPeuFXCt191Pn9+xzTx3LBGz6cGgJs4bvmdpABMiyunk7mYZer6asAoiOi5J0vl/3AbziGOGZx6jQx5OPiwUAAABleyJvcmlnaW4iOiJodHRwczovL21lY2FuaWNhc2NpZW5jZS5mcjo0NDMiLCJmZWF0dXJlIjoiV2ViR1BVIiwiZXhwaXJ5IjoxNjYzNzE4Mzk5LCJpc1N1YmRvbWFpbiI6dHJ1ZX0=';
+    document.head.append(otMeta);
+
     // Check WebGPU compatibility
     if (navigator.gpu == undefined)
         console.log("wsh");
